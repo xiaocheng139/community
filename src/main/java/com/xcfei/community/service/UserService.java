7a7cf58956bd33252e262ee67027affd399fa0bd
xiaocheng <61fxc.job@gmail.com>
2023-10-25 22:54:53 +1100

    Initial commit

---
+++ src/main/java/com/xcfei/community/service/UserService.java
@@ -0,0 +1,4 @@
+package com.xcfei.community.service;
+
+public class UserService {
+}
---
+++ src/test/java/com/xcfei/community/MapperTest.java
@@ -0,0 +1,30 @@
+package com.xcfei.community;
+
+import com.xcfei.community.dao.DiscussPostMapper;
+import com.xcfei.community.entity.DiscussPost;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTest {
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Test
+    public void testSelectPosts() {
+        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(0, 0, 10);
+        for (DiscussPost post: list) System.out.println(post);
+
+        int rows = discussPostMapper.selectDiscussPostRows(0);
+        System.out.println(rows);
+    }
+}
