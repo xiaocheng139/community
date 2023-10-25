257ceeeca7b8e1a6c4063f9e30ddd9ca643c93f9
xiaocheng <61fxc.job@gmail.com>
2023-10-25 23:32:47 +1100

    static contents + index page

--- src/main/java/com/xcfei/community/service/UserService.java
+++ src/main/java/com/xcfei/community/service/UserService.java
@@ -1,4 +1,16 @@
 package com.xcfei.community.service;
 
+import com.xcfei.community.dao.UserMapper;
+import com.xcfei.community.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    public User findUserById(int id) {
+        return userMapper.selectById(id);
+    }
 }
