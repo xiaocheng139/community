25eb00a76ef9e7c1588f40d61d148169c54768ba
xiaocheng <61fxc.job@gmail.com>
2023-10-27 23:26:44 +1100

    Completed mail functionality

---
+++ src/test/java/com/xcfei/community/MailTest.java
@@ -0,0 +1,37 @@
+package com.xcfei.community;
+
+import com.xcfei.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTest {
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("dummy@gmail.com", "Test", "Welcome");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(context);
+        mailClient.sendMail("dummy@gmail.com", "HTML", content);
+    }
+}
